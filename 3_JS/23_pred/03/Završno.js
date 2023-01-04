8c68032be591d8fca2801f1777a61cb71145dcf9
Sanja Drenjancevic <sdrenjan@gmail.com>
2023-01-04 11:03:12 +0100

    23_pred

---
+++ 3_JS/23_pred/03/Završno.js
@@ -0,0 +1,22 @@
+/*
+* Inicijalizacija varijabli?
+*/
+var first_name = "Tony";
+var first_name = "John";
+var lastName = "Doe";
+var price = 0;
+var discount = 78;
+var fullPrice = 0;
+
+var break2 = [];
+
+var $myObject = {};
+
+console.log(first_name);
+console.log(lastName);
+console.log(price);
+console.log(discount);
+console.log(fullPrice);
+
+// Kraj mog
+//programa

5f80384519cb322397d6f218332edc5e1df98f2a
Sanja Drenjancevic <sdrenjan@gmail.com>
2023-01-22 17:10:40 +0100

    update 30_pred

--- 3_JS/30_pred/19 - Classes/01/script.js
+++ 3_JS/30_pred/19 - Classes/01/script.js
@@ -8,7 +8,7 @@ class Character {
     }
 
     basicInfo() {
-        return `Character ${this.firstName} ${this.lastName} from ${this.birthplace} is ${this.age} years old.`
+        return `Character ${this.firstName} ${this.lastName} from ${this.birthplace} is ${this.age} years old.`;
     }
 
     quote() {
@@ -19,12 +19,12 @@ class Character {
         if (this.age >= 18) {
             return `${this.firstName} is not underage.`;
         } else {
-            return `${this.firstName} is underage.`
+            return `${this.firstName} is underage.`;
         }
     }
 }
 
-class Task extends Character {
+class CharacterTask extends Character {
     constructor(firstName, lastName, birthplace, age, virtue, job, weapons) {
         super(firstName, lastName, birthplace, age, virtue)
         this.job = job;
@@ -32,8 +32,10 @@ class Task extends Character {
     }
 
     task() {
-        return `${this.firstName} is ${this.job} and uses ${this.weapons}`
+        return `${this.firstName} is ${this.job} and uses ${this.weapons}`;
     }
 }
 
-console.log(basicInfo());
+let character1 = new CharacterTask('Zidane', 'Tribal', 'Bran Bal', 16, `"You don't need a reason to help people."`, 'Thief', ['short swords', 'daggers']);
+
+console.log(task(character1));

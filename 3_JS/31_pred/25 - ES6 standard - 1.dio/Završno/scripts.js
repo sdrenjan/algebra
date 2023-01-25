401b072ab9ccd3e9b487ba37028f7f43610c29e5
Sanja Drenjancevic <sdrenjan@gmail.com>
2023-01-25 21:21:58 +0100

    33_pred

--- 3_JS/31_pred/25 - ES6 standard - 1.dio/Završno/scripts.js
+++ 3_JS/31_pred/25 - ES6 standard - 1.dio/Završno/scripts.js
@@ -1,9 +1,13 @@
 "use strict";
 
 // 1.
-a = 78;
-b = 034;
-myObject = { property1: 1, property2: 2, property1: 1 };
+var a = 78;
+var b = 034;
+var myObject = {
+  property1: 1,
+  property2: 2,
+  property1: 1
+};
 
 // 2.
 const users = [
@@ -17,7 +21,7 @@ const allUsers = [...users, user];
 console.log(allUsers);
 
 // 3.
-for (let i = 0; i < 10; i++) {
+for (var i = 0; i < 10; i++) {
   setTimeout(function () {
     console.log('The number is ' + i);
   }, 1000);
@@ -25,4 +29,4 @@ for (let i = 0; i < 10; i++) {
 
 console.log(i);
 
-// let vraca i is not defined jer je zadnja linija van scopea, a var vraca 10 (i to ispisano 10 puta number is 10 zbog delaya)
+// let vraca brojeve od 1 do 10, a zadnji i is not defined jer je zadnja linija van scopea, a var vraca na kraju broj 10 i 10 nakon 1 sekunde
---
+++ 3_JS/33_pred - TODO app/script.js
@@ -0,0 +1,124 @@
+/**
+ * ANIMATE THE INTRO LOGO 
+ */
+
+anime({
+    targets: '.line-logo .lines path',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutSine',
+    duration: 1500,
+    delay: function (el, i) { return i * 250 },
+    direction: 'alternate',
+    loop: true
+});
+
+// setTimeout(() => {
+//     document.getElementsByClassName('loading')[0].style.display = 'none';
+// }, 3000)
+
+/**
+ * MODAL 
+ */
+var modal = document.getElementById('modal');
+var button = document.getElementById('button');
+var span = document.getElementsByClassName('close-modal')[0];
+
+// When a user clicks 'not you?', show them a modal
+button.onclick = function () {
+    modal.style.display = 'block';
+}
+
+span.onclick = function () {
+    modal.style.display = 'none';
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        // console.log(event.target) // Object reference
+        modal.style.display = 'none';
+    }
+}
+
+/*
+ * FOOTER
+ */
+
+const year = new Date().getFullYear();
+document.getElementById('year').innerText = year;
+
+/*
+ * GREETING
+ */
+
+const greeting = document.getElementById('greeting');
+const hour = new Date().getHours();
+const welcomeTypes = ['Good Morning', 'Good Afternoon', 'Good Evening'];
+
+let welcomeText = '';
+
+if (hour < 12) {
+    welcomeText = welcomeTypes[0];
+} else if (hour < 18) {
+    welcomeText = welcomeTypes[1];
+} else {
+    welcomeText = welcomeTypes[2];
+}
+
+greeting.innerHTML = `${welcomeText}`;
+
+/*
+ * DATE
+ */
+
+const options = { weekday: 'long', month: 'long', day: 'numeric' };
+let date = new Date();
+document.getElementById('date').innerHTML = date.toLocaleDateString('en-US', options);
+
+/*
+ * SET USER'S NAME
+ */
+
+function setUser() {
+    let name = document.getElementById('name').value;
+    localStorage.setItem('user', name);
+    let modal = document.getElementById('modal');
+    modal.style.display = 'none';
+}
+
+(function showUser() {
+    let user = localStorage.getItem('user');
+    document.getElementById('user').innerText = `${!user ? 'Hey there stranger' : user}`;
+})();
+
+/*
+ * TO DO LIST
+ */
+
+// Create <span> add class='close' to each <span> 
+const myNodeList = document.getElementsByTagName('li');
+var i;
+for (i = 0; i < myNodeList.length; i++) {
+    const span = document.createElement('span');
+    const text = document.createTextNode('\u00D7');
+    span.className = 'close';
+    span.appendChild(text);
+    myNodeList[i].appendChild(span);
+}
+
+// Get each <span> with class='close', add event listener on them, once they are being clicked, remove them from DOM
+const close = document.getElementsByClassName('close');
+var i;
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+        const div = this.parentElement;
+        div.remove();
+    }
+}
+
+// Add a 'checkmark' next to each task, and strikethrough each of them
+const list = document.querySelector('ul');
+list.addEventListener('click', function (event) {
+    if (event.target.tagName.toLowerCase() == 'li') {
+        event.target.classList.toggle('checked');
+    }
+}, false)

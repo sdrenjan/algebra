6aee3a253c3ce804e011f5a33f0562504f66a119
Sanja Drenjancevic <sdrenjan@gmail.com>
2023-02-03 10:10:53 +0100

    36_pred

---
+++ 3_JS/my TODO app/script.js
@@ -0,0 +1,141 @@
+//intro logo animation
+anime({
+    targets: '.line-logo .lines path',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutSine',
+    duration: 1500,
+    delay: function (el, i) { return i * 250 },
+    direction: 'alternate',
+    loop: true
+});
+
+//setTimeout(() => {
+//    document.getElementsByClassName('loading')[0].style.display = 'none';
+//}, 3000);
+
+
+//modal
+var modal = document.getElementById('modal');
+var button = document.getElementById('button');
+var span = document.getElementsByClassName('close-modal')[0];
+
+button.onclick = function () {
+    modal.style.display = 'block';
+}
+
+span.onclick = function () {
+    modal.style.display = 'none';
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
+
+
+//footer
+const year = new Date().getFullYear();
+document.getElementById('year').innerText = year;
+
+
+//greeting
+const greeting = document.getElementById('greeting');
+const hour = new Date().getHours();
+const welcomeTypes = ['Good Morning', 'Good Afternoon', 'Good Evening'];
+
+let welcomeText = '';
+
+if (hour < 12) {
+    welcomeText = welcomeTypes[0];
+} else if (hour < 18) {
+    welcomeText = welcomeTypes[1];
+} else {
+    welcomeText = welcomeTypes[2];
+}
+
+greeting.innerHTML = welcomeText;
+
+
+//date
+const options = { weekday: 'long', month: 'long', day: 'numeric' };
+let date = new Date();
+document.getElementById('date').innerHTML = date.toLocaleDateString('en-US', options);
+
+
+//set user name
+function setUser() {
+    let name = document.getElementById('name').value;
+    localStorage.setItem('user', name);
+    let modal = document.getElementById('modal');
+    modal.style.display = 'none';
+}
+
+(function showUser() {
+    let user = localStorage.getItem('user');
+    document.getElementById('user').innerText = !user ? 'Hey there stranger' : user;
+})();
+
+
+//to do list
+const myNodeList = document.getElementsByTagName('li');
+var i;
+for (i = 0; i < myNodeList.length; i++) {
+    const span = document.createElement('span');
+    const text = document.createTextNode('\u00D7');
+    span.className = 'close';
+    span.appendChild(text);
+    myNodeList[i].appendChild(span);
+}
+
+const close = document.getElementsByClassName('close');
+var i;
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+        const div = this.parentElement;
+        div.remove();
+    }
+}
+
+//add a checkmark and striketrough for each task
+const list = document.querySelector('ul');
+list.addEventListener('click', function (event) {
+    if (event.target.tagName === 'LI') {
+        event.target.classList.toggle('checked');
+    }
+}, false)
+
+function newTask() {
+    const li = document.createElement('li');
+    let inputValue = document.getElementById('inputField').value;
+    const task = document.createTextNode(inputValue);
+    const error = document.getElementById('error');
+    li.appendChild(task);
+
+    if (inputValue === '') {
+        error.style.display = 'inline';
+        setTimeout(function () {
+            error.style.display = 'none';
+        }, 3000)
+    } else {
+        document.getElementById('list').appendChild(li);
+    }
+
+    document.getElementById('inputField').value = '';
+
+    const span = document.createElement('span');
+    const txt = document.createTextNode('\u00D7');
+
+    span.className = 'close';
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function () {
+            const div = this.parentElement;
+            div.remove();
+        }
+    }
+}
+
+
